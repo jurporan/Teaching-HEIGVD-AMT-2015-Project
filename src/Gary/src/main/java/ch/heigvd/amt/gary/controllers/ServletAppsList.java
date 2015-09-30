e217a6f6089efd2e695e476c426ff18844442fea
Olivier Liechti <olivier.liechti@wasabi-tech.com>
2015-09-30 20:57:26 +0200

    Feedback from OLI

--- src/Gary/src/main/java/ch/heigvd/amt/gary/controllers/ServletAppsList.java
+++ src/Gary/src/main/java/ch/heigvd/amt/gary/controllers/ServletAppsList.java
@@ -86,3 +86,21 @@ public class ServletAppsList extends HttpServlet {
    }// </editor-fold>
 
 }
+
+/**
+ * FEEDBACK:
+ * - typo: Your apps (not You apps)
+ * 
+ * - in many applications, people prefer to use AccountServlet rather than ServletAccount.
+ * 
+ * - the commit message says that this page does not work. You probably know that it is because
+ *   AppsDataStore.getAllApps() is not implemented and throws an exception. If you want my opinion, it would
+ *   have been worthwhile to spend time (we are talking about 2 minutes) to return the list of apps from
+ *   the session bean before making this commit (and in addition to make a link to this page from the
+ *   default index page). This way, everybody in the team would have been able to get the update, build and
+ *   run locally. Even if the functionality is very small, seeing progress on your machine and seeing step-by-step
+ *   improvements has a very positive impact in order to get your on the same page, in the same rythm. It is really
+ *   about psychology and coordination. In software engineering, people often talk about "baby steps": I do a small
+ *   code change, I pull other devs modifs from remove git, I test, it works, I commit and I push. Try it and
+ *   you will see that it put you in a very positive and productive mood.
+ */
--- src/Gary/src/main/webapp/index.jsp
+++ src/Gary/src/main/webapp/index.jsp
@@ -15,3 +15,13 @@
       <h1>Hello Fromage !</h1>
    </body>
 </html>
+
+<%--
+  FEEDBACK: you have an example to redirect the user to the default page. You should do that quite quickly.
+            Why? Think about the perception of the team. Is it not nice to see a nice, working page instead of
+            a default blank page? I know that it may look futile, but it will increase your confidence in the
+            quality of your project and in your progress (otherwise, you will have the feeling that you are 
+            always in a "broken", "unfinished" state. It is better to have a system that provides very, very
+            limited functionality, but which is solid and polished, rather than a system full of half-baked code.
+
+--%>
