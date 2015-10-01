db4ea27e089ee8b7f943544d262403a897cdce25
edri <miguel.santamaria@heig-vd.ch>
2015-10-01 15:38:34 +0200

    Finalisation de la page 'Your apps'

--- src/Gary/src/main/java/ch/heigvd/amt/gary/controllers/ServletAppsList.java
+++ src/Gary/src/main/java/ch/heigvd/amt/gary/controllers/ServletAppsList.java
@@ -1,7 +1,6 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * The servlet that manage and show the list of applications.
+ * Created on 01.10.2015 by Miguel Santamaria
  */
 package ch.heigvd.amt.gary.controllers;
 
--- src/Gary/src/main/java/ch/heigvd/amt/gary/models/App.java
+++ src/Gary/src/main/java/ch/heigvd/amt/gary/models/App.java
@@ -1,7 +1,6 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Represent the objet that define an application.
+ * Created on 01.10.2015 by Miguel Santamaria
  */
 package ch.heigvd.amt.gary.models;
 
