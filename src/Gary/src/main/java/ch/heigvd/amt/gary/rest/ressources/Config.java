9f79e5424ace170a03de1e79f555419a1ecd529f
benoistwolleb <benoist.wolleb@heig-vd.ch>
2015-12-04 18:13:31 +0000

    ça plante plus mais ça marche pas

--- src/Gary/src/main/java/ch/heigvd/amt/gary/rest/ressources/Config.java
+++ src/Gary/src/main/java/ch/heigvd/amt/gary/rest/ressources/Config.java
@@ -1,9 +1,13 @@
 package ch.heigvd.amt.gary.rest.ressources;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.jaxrs.json.JacksonJaxbJsonProvider;
+import com.fasterxml.jackson.module.jaxb.JaxbAnnotationModule;
 import java.util.Set;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.ws.rs.core.Application;
+import jersey.repackaged.com.google.common.collect.ImmutableSet;
 
 
 // Just a temporary config class for testing JAX-RS.
