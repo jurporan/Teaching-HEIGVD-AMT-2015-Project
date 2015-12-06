257b20849b5a7698140900b967e0d5d223223e21
melhk <melanie.huck@alumni.epfl.ch>
2015-12-06 19:19:16 +0100

    test

--- tests/TestsConcurrence/src/main/java/com/mycompany/testsconcurrence/TestClient.java
+++ tests/TestsConcurrence/src/main/java/com/mycompany/testsconcurrence/TestClient.java
@@ -1,9 +1,10 @@
-    // Test des events
+/*
+ * Test class
+*/
 
 package com.mycompany.testsconcurrence;
-
-
-
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ExecutorService;
@@ -23,35 +24,112 @@ import org.glassfish.jersey.jackson.JacksonFeature;
 public class TestClient {
     
     private final Client client;
-    private final int numberOfConcurrentThreads;
-    
+    private String apiKey;
+    private final int numberOfConcurrentThreads; 
     private static final Logger LOG = Logger.getLogger(TestClient.class.getName());
+    private ExpectedState expectedState = null;
+    private final long addedPoints = 1;
+    private final long deductedPoints = 5;
     
     
     /**
      * Constructor
      * @param numberOfConcurrentThreads Number of concurrent threads
      */
-    public TestClient(int numberOfConcurrentThreads)
+    public TestClient(String apiKey, int numberOfConcurrentThreads)
     {
         this.client = ClientBuilder.newClient().register(JacksonFeature.class);
         this.numberOfConcurrentThreads = numberOfConcurrentThreads;  
+        this.apiKey = apiKey;
     }
 
-    private void createEndUser()
+    private void createEndUser(int id)
     {
-        EndUserDTO endUser = new EndUserDTO(2342432);
+        EndUserDTO endUser = new EndUserDTO();
+        endUser.setId(id);
         final WebTarget target = client
                 .target("http://localhost:8080/Gary/api")
-                .path("applications/6dfc4960-a27b-4387-a1f3-c3fef11f7d13/users");     
-        Response response = target.request().post(Entity.json(endUser));     
+                .path("applications/" + apiKey + "/users");     
+        Response response = target.request().post(Entity.json(endUser));  
+        if(response.getStatus() < 200 || response.getStatus() >= 300)
+        {
+            LOG.log(Level.INFO, "The server was not able to create a user", 
+                    new Object[]{response.getStatus() + " " + response.getStatusInfo()});
+        }
+    }
+    
+    private void createRule(String typeOfEvent, long ruleParameter, boolean penalty, int minValue,
+            int maxValue, byte rewardType)
+    {
+        RuleDTO rule = new RuleDTO(typeOfEvent, ruleParameter, 
+                penalty, minValue, maxValue, rewardType);
+        
+        final WebTarget target = client
+            .target("http://localhost:8080/Gary/api")
+            .path("applications/" + apiKey + "/rules");   
+
+        Response response = target.request().post(Entity.json(rule));
+        if(response.getStatus() < 200 || response.getStatus() >= 300)
+        {
+            LOG.log(Level.INFO, "The server was not able to create a new rule", 
+                    new Object[]{response.getStatus() + " " + response.getStatusInfo()});
+        }
+    }
+    
+    private long getPointsFromEndUser(long userID)
+    {
+        long points = 0;
+        final WebTarget target = client
+                .target("http://localhost:8080/Gary/api")
+                .path("applications/" + apiKey + "/users/" 
+                        + userID + "/reputation");  
+      
+        Response response = target.request().get();
+        if(response.getStatus() < 200 || response.getStatus() >= 300)
+        {
+            LOG.log(Level.INFO, "Cannot get points from user", 
+                    new Object[]{response.getStatus() + " " + response.getStatusInfo()});
+        }
+        else
+        {
+            System.out.println(response.toString());
+            ReputationDTO reputation = ReputationDTO
+                    .fromEntity((Reputation)response
+                    .readEntity(Reputation.class));
+            points = reputation.getPoints();
+        }
+        return points;
+    }
+    
+    private void postEvent(String eventType, long userID)
+    {
+        EventDTO event = new EventDTO();
+        final WebTarget target = client
+            .target("http://localhost:8080/Gary/api")
+            .path("applications/" + apiKey + "/users/" 
+                        + userID + "/events");   
+                
+        event.setType(eventType);
+        event.setParameter(null);
+        
+        Response response = target.request().post(Entity.json(event));
+        
+        if(response.getStatus() < 200 || response.getStatus() >= 300)
+        {
+            LOG.log(Level.INFO, "The server was not able to post an event", 
+                    new Object[]{response.getStatus() + " " + response.getStatusInfo()});
+        }
+        else
+        {
+            expectedState.logEvent(event);
+        }
     }
     
     private List<EndUserDTO> getUserListFromServer()
     {
         final WebTarget target = client
                 .target("http://localhost:8080/Gary/api")
-                .path("applications/6dfc4960-a27b-4387-a1f3-c3fef11f7d13/users"); 
+                .path("applications/" + apiKey +"/users"); 
         
         GenericType<List<EndUserDTO>> accountsListType = new GenericType<List<EndUserDTO>>(){};
         return target.request().get(accountsListType);
@@ -60,38 +138,64 @@ public class TestClient {
     private void test()
     {
         ExecutorService executor = Executors.newFixedThreadPool(numberOfConcurrentThreads);
-        
-        final WebTarget target = client
-                .target("http://localhost:8080/Gary/api")
-                .path("applications/6dfc4960-a27b-4387-a1f3-c3fef11f7d13/users");
-        
-        // Create one user
-        createEndUser();
+          
+        try
+        {
+            // Create one rule to add points
+            createRule("messagePosted", addedPoints, false, 0, 0, (byte)1);
+        }
+        catch (Exception e) { System.err.println(e); };
 
-        // Check if there is at least one user
+        try
+        {
+            // Create one rule to deduct points
+            createRule("insultingSomeone", deductedPoints, true, 0, 0, (byte)1);
+        }
+        catch (Exception e) { System.err.println(e); };
+
+        // Check if there is at least one user, and create one if there is no user.
         List<EndUserDTO> userList = getUserListFromServer();
         if(userList.isEmpty()) 
         { 
-            createEndUser();
+            createEndUser(321);
         }
         
         // Get the id of the first end user of the list
-        EndUserDTO endUser = userList.get(0);
+        final long userID = userList.get(0).getId();
+        long userPoints = getPointsFromEndUser(userID);
+        
+        expectedState = new ExpectedState(userPoints, addedPoints, deductedPoints);
 
-        // Add points to the user
-        Runnable task = new Runnable() 
+        // Adds points to the user
+        Runnable addTask = new Runnable() 
         {
             @Override 
             public void run()
             {
-                
-                
+                for (int i = 0; i < 500; ++i)
+                {
+                    postEvent("messagePosted", userID);
+                }
             }
         };       
-        executor.execute(task);
         
+        // Deducts points to the user
+        Runnable deductTask = new Runnable() 
+        {
+            @Override 
+            public void run()
+            {
+                for (int i = 0; i < 100; ++i)
+                {
+                    postEvent("insultingSomeone", userID);
+                } 
+            }
+        }; 
         
-        
+        executor.execute(addTask);
+        executor.execute(deductTask);
+        LOG.log(Level.INFO, "All tasks have been submitted to the executor and will"
+                + " be processed by {0} concurrent threads.", numberOfConcurrentThreads);
         
         try
         {
@@ -104,12 +208,39 @@ public class TestClient {
             Logger.getLogger(TestClient.class.getName()).log(Level.SEVERE, null, e);
         }
         LOG.info("Done.");
+              
+        List<String> errors = validateExpectedAgainstActualState(userID);
+        LOG.info("Errors: " + errors.toString());
+        
+        LOG.info(expectedState.toString());
     }
 
     
     public static void main(String[] args) {
-        // Run the test with 10 threads
-        (new TestClient(10)).test();        
+        
+        BufferedReader buffer = new BufferedReader(new InputStreamReader(System.in));
+        try
+        {         
+            // Enter an existing api key
+            // String apiKey = buffer.readLine().trim();
+            String apiKey = "7f1bfd82-5fcf-4c4b-a362-25d79c99331b";
+            // Run the test with 100 threads
+            (new TestClient(apiKey, 100)).test(); 
+        } catch (Exception e) { System.err.println(e); }   
     }
     
+      
+    private List<String> validateExpectedAgainstActualState(long userID) {
+
+        List<String> errors = new ArrayList<>();
+        
+        LOG.log(Level.INFO, "Expected expected points for user: ", expectedState.getExpectedPoints());
+        if(expectedState.getExpectedPoints() != getPointsFromEndUser(userID))
+        {
+            errors.add("The number of final points is not the one expected: " 
+                    + expectedState.getExpectedPoints() + " vs "
+                    + getPointsFromEndUser(userID));
+        }
+        return errors;
+  }
 }
